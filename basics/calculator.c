64868a92471868c810b9147f4ae69e86f72ddccb
YUVRAJ PRATAP SINGH <144945455+yuvraj0702@users.noreply.github.com>
2023-09-17 22:07:00 +0530

    Update calculator.c

--- basics/calculator.c
+++ basics/calculator.c
@@ -1,32 +1,32 @@
-  #include <stdio.h>
+#include <stdio.h>
 
 int main()
- {
+{
+    int a,b,option;
+    printf("Enter first number:");
+    scanf("%d",&a);
 
-  char op;
-  double first, second;
-  printf("Enter an operator (+, -, *, /): ");
-  scanf("%c", &op);
-  printf("Enter two operands: ");
-  scanf("%lf %lf", &first, &second);
+    printf("Enter second number:");
+    scanf("%d",&b);
 
-  switch (op) {
-    case '+':
-      printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
-      break;
-    case '-':
-      printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
-      break;
-    case '*':
-      printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
-      break;
-    case '/':
-      printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
-      break;
-    // operator doesn't match any case constant
-    default:
-      printf("Error! operator is not correct");
-     return 0;
-
+    printf("\npress 1 for additon");
+    printf("\npress 2 for substraction");
+    printf("\npress 3 for multiplication");
+    printf("\npress 4 for divisson");
+    scanf("%d",&option);
+    switch (option)
+{
+    case 1:
+    printf("sum = %d",a+b);
+    break; 
+    case 2:
+    printf("substraction = %d",a-b);
+    break;
+    case 3:
+    printf("multiplication = %d",a*b);
+    break;
+    case 4:
+    printf("divissson = %f",(float)a/b);
+    break;
 }
- }
+} 
