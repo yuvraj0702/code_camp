d190e8a6d730597f9f88055cf4f5353a3b0171e1
YUVRAJ PRATAP SINGH <144945455+yuvraj0702@users.noreply.github.com>
2023-09-21 11:17:56 +0530

    Create greatest number.c

---
+++ basics/greatest number.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main ()
+{
+    int x,y,z;
+
+    printf ("Enter x,");
+    scan;
+
+    if  (x>y && x>z);
+
+    {
+        printf("x is greatest ");
+    
+    }
+
+    else if (y>x && Y>Z);
+     {
+        printf("y is greatest");
+     }
+
+     else  (z>x && z>y);
+     {
+        printf("z is greatest");
+      }
+} 
